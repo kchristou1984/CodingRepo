63d0c731a207a48c6b051b8764d9e84c01b61a10
Kostas <kchristou1984@gmail.com>
2024-05-27 00:35:08 +0300

    JavaFinal

--- Αντικειμενοστραφής προγραμματισμός/Final Assignment/Library.java
+++ Αντικειμενοστραφής προγραμματισμός/Final Assignment/Library.java
@@ -5,6 +5,7 @@ public class Library {
 
     ArrayList<Book> bookList = new ArrayList<>();
     ArrayList<Reader> readerList = new ArrayList<>();
+    ArrayList<Book> lendedBookList = new ArrayList<>();
 
 
     public void addBook(String ISBN, String title, String author, String genre, String position){
@@ -14,7 +15,13 @@ public class Library {
     public void addReader(String afm,String name,int age,String category){
         readerList.add(new Reader(afm, name, age, category));
     }
+    public void lendBook(Book book){
+        lendedBookList.add(Book book);
+    }
 
+    public void returnBook(){
+
+    }
     public void printLendedBooks(){
         //
     }
